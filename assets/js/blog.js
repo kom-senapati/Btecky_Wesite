e5e7242e40bf5d45c33cc89a47523840ba5a07a7
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-07-01 17:01:04 +0530

    Update blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -11,7 +11,7 @@ const exp = [
     date:"01-07-2024",
     desp: "Embarking on a new learning journey can be exciting, especially when you have access to high-quality, free courses. One platform offers a variety of free courses for beginners, complete with certificates and sometimes even additional goodies. These courses cover diverse topics such as programming, data science, and providing foundational knowledge, practical skills, and industry-recognized certifications to boost your career.",
     desp2: "",
-    desp3: "These free courses also provide an excellent opportunity to for you guys.\nLink To Video: https://youtu.be/7JKSrp80TFY \nLink To Event Website: https://letsupgrade.in/programs#events",
+    desp3: "These free courses also provide an excellent opportunity for you guys.\nLink To Video: <a href=\"https://youtu.be/7JKSrp80TFY\" target=\"_blank\">https://youtu.be/7JKSrp80TFY</a><br>Link To Event Website: <a href=\"https://letsupgrade.in/programs#events\" target=\"_blank\">https://letsupgrade.in/programs#events</a>",
   }
 ];
 
