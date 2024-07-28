305b878a5275005eab0baf1eb471f4c089294d7c
Kushal Agrawal <98145879+kushal34712@users.noreply.github.com>
2024-07-28 13:02:21 +0530

    Update points-calculator.js

--- assets/js/points-calculator.js
+++ assets/js/points-calculator.js
@@ -290,16 +290,16 @@ document.getElementById('calculateButton').addEventListener('click', async funct
   
   
   
-  // document.addEventListener('contextmenu', function(e) {
-  //   e.preventDefault();
-  // });
-  // document.addEventListener('keydown', function(e) {
-  //   if (e.key === 'F12' || 
-  //       (e.ctrlKey && e.shiftKey && e.key === 'I') || 
-  //       (e.ctrlKey && e.shiftKey && e.key === 'J') || 
-  //       (e.ctrlKey && e.key === 'U')) {
-  //       e.preventDefault();
-  //   }
-  // });
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'F12' || 
+        (e.ctrlKey && e.shiftKey && e.key === 'I') || 
+        (e.ctrlKey && e.shiftKey && e.key === 'J') || 
+        (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+    }
+  });
   
   
